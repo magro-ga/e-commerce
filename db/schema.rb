838b0c11e1bb5998b40968b4ddccae209b5cc14a
magro-ga <gabas9630@gmail.com>
2025-02-17 15:51:05 -0300

    made configuration adjustments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_16_185514) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_17_153112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_16_185514) do
   end
 
   add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
