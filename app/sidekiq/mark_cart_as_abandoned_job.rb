b13ed927cdf92be72f3a2e516dcf96cfdfb992e9
magro-ga <gabas9630@gmail.com>
2025-02-17 01:57:28 -0300

    added the jobs to set carts abandoned

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,16 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
+  queue_as :default
 
   def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+    # Marca carrinhos como abandonados se não tiverem interação há mais de 3 horas
+    Cart.where(status: :active)
+        .where("last_interaction_at <= ?", Cart::CONSIDERED_ABANDONED_TIMESPAN.ago)
+        .find_each(&:mark_as_abandoned)
+
+    # Remove carrinhos abandonados há mais de 7 dias
+    Cart.where(status: :abandoned)
+        .where("last_interaction_at <= ?", Cart::REMOVE_ABANDONED_CART_TIMESPAN.ago)
+        .find_each(&:destroy)
   end
 end
--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -1,3 +1,37 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
+  describe "#perform" do
+    let!(:recent_cart) { create(:cart, last_interaction_at: 2.hours.ago) }
+    let!(:old_cart) { create(:cart, last_interaction_at: 4.hours.ago) }
+    let!(:abandoned_cart) { create(:cart, :old_abandoned) }
+
+    it "marca carrinhos antigos como abandonados" do
+      expect(old_cart.status).to eq("active")
+
+      MarkCartAsAbandonedJob.new.perform
+
+      old_cart.reload
+      expect(old_cart.status).to eq("abandoned")
+    end
+
+    it "não altera carrinhos recentes" do
+      expect(recent_cart.status).to eq("active")
+
+      MarkCartAsAbandonedJob.new.perform
+
+      recent_cart.reload
+      expect(recent_cart.status).to eq("active")
+    end
+
+    it "remove carrinhos abandonados há mais de 7 dias" do
+      expect(Cart.exists?(abandoned_cart.id)).to be_truthy
+
+      MarkCartAsAbandonedJob.new.perform
+
+      expect(Cart.exists?(abandoned_cart.id)).to be_falsey
+    end
+  end
 end
