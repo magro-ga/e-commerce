4c0ee336d03265c1b19dede6b5d9b65947abc632
magro-ga <gabas9630@gmail.com>
2025-02-17 01:59:23 -0300

    refactored tests and configs

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -1,4 +1,6 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe CartsController, type: :routing do
   describe 'routes' do
@@ -7,11 +9,15 @@ RSpec.describe CartsController, type: :routing do
     end
 
     it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
+      expect(post: '/cart').to route_to('carts#create')
     end
 
-    it 'routes to #add_item via POST' do
-      expect(post: '/cart/add_item').to route_to('carts#add_item')
+    it 'routes to #add_items via POST' do
+      expect(post: '/cart/add_items').to route_to('carts#create')
+    end
+
+    it 'routes to #delete_items via DELETE' do
+      expect(delete: '/cart/1').to route_to('carts#destroy', product_id: '1')
     end
   end
-end 
+end
